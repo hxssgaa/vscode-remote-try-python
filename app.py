8007d6a1c3b7e8ce83d3eaae7fd5149b06e63de5
wuwei.hx <wuwei.hx@alibaba-inc.com>
2019-05-20 09:24:54 +0000

    add torch code

--- app.py
+++ app.py
@@ -3,9 +3,15 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
+import torch
+import numpy as np
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    x = np.array(list(range(200)))
+    x = torch.tensor(x, dtype=torch.float64, device=device)
+    y = x ** 2
     return app.send_static_file("index.html")
